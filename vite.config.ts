eb76f4d848f57c00b5494f0a1fc731a130897c09
Felix <53119101+felix9611@users.noreply.github.com>
2024-09-30 16:03:21 -0400

    test

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
       usePolling: true
     }
   },
-  base: '/torontoheartcon/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
